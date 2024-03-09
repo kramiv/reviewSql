612eced18530d393104a5503caf1c80912cccd17
kramiv <57569429+kramiv@users.noreply.github.com>
2024-03-09 17:27:42 -0600

    Update hydrateTables.sql

--- sqlPractice/createDatabase_createTable/hydrateTables.sql
+++ sqlPractice/createDatabase_createTable/hydrateTables.sql
@@ -1,17 +1,17 @@
 USE recipe_macros_database;
 
 INSERT INTO recipe (recipe_name, minutes_of_prep, hrs_of_prep) VALUES 
-	('sandwich', 30, 0.0), 
-	('turkey', 31, 25.2),
-	('hamburger', 1, 10),
-	('bowl of store-bought olives', 0, 0),
-	('Hottodoggu', 10, 0),
-	('Hanbaga', 15, 0),
-	('Chips', 1, 0),
-	('Bob\'s Burgers', 1, 0);
+    ('sandwich', 30, 0.0), 
+    ('turkey', 31, 25.2),
+    ('hamburger', 1, 10),
+    ('bowl of store-bought olives', 0, 0),
+    ('Hottodoggu', 10, 0),
+    ('Hanbaga', 15, 0),
+    ('Chips', 1, 0),
+    ('Bob\'s Burgers', 1, 0);
 
 INSERT INTO meal (date_of_meal, servings_consumed, fk_recipe) VALUES 
-	('2008-11-11', 3, 1),
+    ('2008-11-11', 3, 1),
     ('2008-11-11', 2, 2),
     ('2008-11-11', 3, 1);
     
@@ -21,7 +21,7 @@ INSERT INTO ingredient (ingredient_name, cals_per_hundred_grams, protein_grams, 
     ('cheddar cheese slice', 90, 8, 5, 8, 1);
 
 INSERT INTO ingredient_recipe (ingredient_id, recipe_id) VALUES
-	(1, 1),
+    (1, 1),
     (2, 1),
     (3, 1);
 
